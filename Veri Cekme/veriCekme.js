d3d11bce20c88a61dafca4bd32848b4b038c3c2a
Muhammed Hukumdar <86476907+hkmdrmuhamd@users.noreply.github.com>
2023-05-05 10:18:04 +0300

    Update veriCekme.js

--- Veri Cekme/veriCekme.js
+++ Veri Cekme/veriCekme.js
@@ -2,8 +2,8 @@ const { Axios, default: axios } = require("axios");
 
 const getAll = async() =>{
     let arr = []
-    const data = await axios.get("https://restcountries.com/v3.1/all")//Link üzerinden verileri alır. Bu
-    //verileri alma işlemi birkaç saniye sürecei için async fonksiyonu ile birkaç saniye bekletme işlemi gerçekleştirilir.
+    const data = await axios.get("https://restcountries.com/v3.1/all")//Link üzerinden verileri alır. Bu verileri alma işlemi birkaç saniye süreceği için async fonksiyonu
+    //ile birkaç saniye bekletme işlemi gerçekleştirilir.
     arr.push(data)//Çektiğimiz verileri dizinin içerisine atma işlemi.
     console.log(data)
     return arr
